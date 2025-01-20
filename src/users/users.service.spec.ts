0a366a52ef44a6c6a7b6b6ac181e6f04f9cb47ab
vanshika-dargan-18 <vanshika.dargan@vonage.com>
2025-01-20 17:50:43 +0530

    code-update: code coverage for other methods findAll, findById, findByEmail,update and delete

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -3,9 +3,14 @@ import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { PasswordService } from 'src/password/password.service';
-import { UserRole } from 'src/roles/enums/user-role.enum';
-import { CreateUserDto } from './dto/create-user.dto';
 import { Repository } from 'typeorm';
+import {
+  createUserDto,
+  user,
+  users,
+  updateUserDto,
+  updatedUser,
+} from './mock/mock-user';
 
 describe('UsersService', () => {
   let userService: UsersService;
@@ -17,6 +22,7 @@ describe('UsersService', () => {
     save: jest.fn(),
     find: jest.fn(),
     findOne: jest.fn(),
+    update: jest.fn(),
     delete: jest.fn(),
   };
 
@@ -47,64 +53,82 @@ describe('UsersService', () => {
     expect(userService).toBeDefined();
   });
 
+  // create user
   describe('create', () => {
     it('should hash the password, create and save the user', async () => {
-      const mockRole = {
-        id: 'SUPERUSER',
-        name: UserRole.SUPER_USER,
-        label: 'Super User',
-      };
-
-      const createUserDto = {
-        parent_id: 0,
-        first_name: 'John',
-        last_name: 'Doe',
-        login_name: 'johndoe',
-        email: 'john.doe@vonage.com',
-        role: mockRole,
-        password: 'abcdef',
-        createdBy: 0,
-      } as CreateUserDto;
-
-      const hashedPassword = 'hashed_abcdef';
-
-      const savedUser = {
-        id: 1,
-        parent_id: 0,
-        first_name: 'John',
-        last_name: 'Doe',
-        login_name: 'johndoe',
-        email: 'john.doe@vonage.com',
-        role: {
-          id: 'SUPERUSER',
-          name: UserRole.SUPER_USER,
-          label: 'Super User',
-        },
-        password: hashedPassword,
-        groups: null,
-        created_at: new Date(),
-        createdBy: 0,
-      } as UserEntity;
-
       jest
         .spyOn(passwordService, 'hashPassword')
-        .mockResolvedValue(hashedPassword);
-      jest
-        .spyOn(userRepository, 'create')
-        .mockReturnValue(savedUser as UserEntity);
-      jest
-        .spyOn(userRepository, 'save')
-        .mockResolvedValue(savedUser as UserEntity);
+        .mockResolvedValue('hashed_abcdef');
+      jest.spyOn(userRepository, 'create').mockReturnValue(user);
+      jest.spyOn(userRepository, 'save').mockResolvedValue(user);
 
       const result = await userService.create(createUserDto);
       const { password, ...rest } = createUserDto;
       expect(passwordService.hashPassword).toHaveBeenCalledWith(password);
       expect(userRepository.create).toHaveBeenCalledWith({
         ...rest,
-        password: hashedPassword,
+        password: 'hashed_abcdef',
       });
-      expect(userRepository.save).toHaveBeenCalledWith(savedUser);
-      expect(result).toEqual(savedUser);
+      expect(userRepository.save).toHaveBeenCalledWith(user);
+      expect(result).toEqual(user);
+    });
+  });
+
+  //get all users
+
+  describe('findAll', () => {
+    it('should return all users', async () => {
+      jest.spyOn(userRepository, 'find').mockResolvedValue(users);
+      const result = await userService.findAll();
+      expect(result).toEqual(users);
+    });
+  });
+
+  // get user by id
+  describe('findById', () => {
+    it('should return a user by id', async () => {
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue(user);
+      const result = await userService.findById(1);
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+        relations: ['role', 'groups'],
+      });
+      expect(result).toEqual(user);
+    });
+  });
+
+  // get user by email
+  describe('findByEmail', () => {
+    it('should return a user by email', async () => {
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue(user);
+      const result = await userService.findByEmail('john.doe@vonage.com');
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { email: 'john.doe@vonage.com' },
+        relations: ['role', 'groups'],
+      });
+      expect(result).toEqual(user);
+    });
+  });
+
+  // update user
+  describe('update', () => {
+    it('should update a user', async () => {
+      jest.spyOn(userRepository, 'update').mockResolvedValue(undefined);
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue(updatedUser);
+
+      const result = await userService.update(1, updateUserDto);
+      expect(userRepository.update).toHaveBeenCalledWith(1, updateUserDto);
+      expect(userRepository.findOne).toHaveBeenCalledWith({ where: { id: 1 } });
+      expect(result).toEqual(updatedUser);
+    });
+  });
+
+  // remove a user
+  describe('remove', () => {
+    it('should remove a user by id', async () => {
+      jest.spyOn(userRepository, 'delete').mockResolvedValue(undefined);
+      await userService.remove(1);
+      expect(userRepository.delete).toHaveBeenCalledWith(1);
     });
   });
 });
